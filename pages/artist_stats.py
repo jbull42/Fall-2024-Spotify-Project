a98f4e340961814246c516c9369f192e623a32d2
Bunando <34804579+Bunando@users.noreply.github.com>
2024-12-03 21:20:16 -0500

    Improved aritst_stats efficiency
    1.5-2 times faster for getting all songs
    Removed api calls for getting avg album length

--- pages/artist_stats.py
+++ pages/artist_stats.py
@@ -50,39 +50,56 @@ def search_for_artist(token, artist_name):
         return None
     return json_result[0]
 
-
+# Will return duplicate songs
 @st.cache_data(ttl=timedelta(weeks=1))
 def get_all_songs_by_artist(_token, artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/albums?limit=50&include_groups=album,single"
     headers = get_auth_header(_token)
-    song_result = []
-    song_result = get_songs_by_artist(url, headers, artist_id, song_result)
+    song_result = get_songs_by_artist(url, headers, artist_id)
     return song_result
 
 
-def get_songs_by_artist(url, headers, artist_id, song_result: list): 
+def get_songs_by_artist(url, headers, artist_id) -> list: 
+    song_result = list()
     # Get all albums from url
     result = get(url, headers=headers)
     json_result = json.loads(result.content)
     album_result = json_result['items']
-    for album in album_result:
-        # Get ids from all albums
-        url = f"https://api.spotify.com/v1/albums/{album['id']}/tracks?limit=50"
-        result = get(url, headers=headers)
-        song_ids = [song['id'] for song in json.loads(result.content)['items']]
-        # Get tracks from song ids
-        url = f"https://api.spotify.com/v1/tracks?ids={','.join(song_ids)}"
-        result = get(url, headers=headers)
-        song_result.extend(json.loads(result.content)['tracks'])
 
-    # Get list of songs with no duplicate song isrc in list
-    songs = [song for n, song in enumerate(song_result) if song['external_ids']['isrc'] not in [prev_song['external_ids']['isrc'] for prev_song in song_result[:n]] and song['artists'][0]['id'] == artist_id]
+    for i in range(0, len(album_result), 20):
+        url = f"https://api.spotify.com/v1/albums?ids={','.join([album['id'] for album in album_result[i:i+20]])}&limit=50"
+        result = get(url, headers=headers)
+        albums = json.loads(result.content)['albums']
+
+        for album in albums:
+            # Get ids from all albums
+            song_ids = [song['id'] for song in album['tracks']['items']]
+            # Get tracks from song ids
+            url = f"https://api.spotify.com/v1/tracks?ids={','.join(song_ids)}"
+            result = get(url, headers=headers)
+            tracks = json.loads(result.content)['tracks']
+            song_result.extend(tracks)
+            if album['tracks']['next'] != None:
+                song_result.extend(get_album_tracks(album['tracks']['next'], headers))
 
     # Add next page of songs if results are paginated
     if json_result['next'] != None:
-        return get_songs_by_artist(json_result['next'], headers, artist_id, songs)
-    return songs
+        song_result.extend(get_songs_by_artist(json_result['next'], headers, artist_id))
+        return song_result
+    return song_result
 
+def get_album_tracks(url, headers):
+    result = get(url, headers=headers)
+    album = json.loads(result.content)
+    song_ids = [song['id'] for song in album['items']]
+    # Get tracks from song ids
+    url = f"https://api.spotify.com/v1/tracks?ids={','.join(song_ids)}"
+    result = get(url, headers=headers)
+    tracks = json.loads(result.content)['tracks']
+    if album['next'] != None:
+        tracks.extend(get_album_tracks(album['next'], headers))
+    return tracks
+    
 
 def print_songs_by_artist(songs):
     for i, song in enumerate(songs):
@@ -98,38 +115,39 @@ def get_average_length(songs) -> str:
 
 
 def get_average_popularity(songs) -> float:
-    # Get ids from all albums
-    # url = f"https://api.spotify.com/v1/artists/{artist_id}"
-    # headers = get_auth_header(_token)
-    # result = get(url, headers=headers)
-    # return json.loads(result.content)['popularity']
     popularity = 0
     for song in songs:
         popularity += song['popularity']
     return popularity/len(songs)
 
-def get_average_album_length(songs, artist_id, _token) -> str:
-    total_time_ms = 0
-    for song in songs:
-        total_time_ms += song['duration_ms']
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/albums?limit=50&include_groups=album"
-    headers = get_auth_header(_token)
-    result = get(url, headers=headers)
-    albums = json.loads(result.content)['items']
-    length = timedelta(milliseconds=total_time_ms/len(albums))
-    return f"{str(length.seconds//60).zfill(2)}:{str(length.seconds%60).zfill(2)}"
+def get_average_album_length(all_songs) -> str:
+    total_length_ms = 0
+    album_list = set()
+    for song in all_songs:
+        if song['album']['album_type'] == 'album':
+            total_length_ms += song['duration_ms']
+            album_list.add(song['album']['id'])
 
-def add_artist(text: str):
+    if len(album_list) != 0:
+        length = timedelta(milliseconds=total_length_ms/len(album_list))
+        return f"{str(length.seconds//60).zfill(2)}:{str(length.seconds%60).zfill(2)}"
+    return None
+
+def add_artist(text: str): 
     artistName = text
     token = get_token()
     artist = search_for_artist(token, artistName if artistName != "" else 'The Beatles')
     st.write(artist['name'])
-    songs = get_all_songs_by_artist(token, artist['id'])
+    all_songs = get_all_songs_by_artist(token, artist['id'])
+
+    # Remove all duplicate song isrc and song's main artist not being the searched for artist in list
+    songs = [song for n, song in enumerate(all_songs) if song['external_ids']['isrc'] not in [prev_song['external_ids']['isrc'] for prev_song in all_songs[:n]] and song['artists'][0]['id'] == artist['id']]
+
     newData = pd.DataFrame({
         "Name": [artist['name']],
         "Average Length": [get_average_length(songs)],
         "Average Song Popularity": [get_average_popularity(songs)],
-        "Average Album Length": [get_average_album_length(songs, artist['id'], token)]
+        "Average Album Length": [get_average_album_length(all_songs)]
     })
     df: pd.DataFrame = st.session_state['df']
     if df.empty or newData['Name'].to_dict()[0] not in df.to_dict()['Name'].values():
